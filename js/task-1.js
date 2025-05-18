f70e0484633c5cbf7b9974ce2210191cb32ac470
Pashka Antonov <pavelcreed222@gmail.com>
2025-05-18 12:09:16 +0300

    task-1

--- js/task-1.js
+++ js/task-1.js
@@ -1,11 +1,12 @@
-function makeTransaction(quantity, pricePerDroid, customerCredits){
-    const totalPrice = quantity * pricePerDroid;
-    if (customerCredits <= totalPrice){
-        return 'Insufficient funds!';
-    }else{
-        return 'You ordered ${quantity} droids worth ${totalPrice} credits!';
-    }
-    }
+function makeTransaction(quantity, pricePerDroid, customerCredits) {
+  const totalPrice = quantity * pricePerDroid;
+
+  if (totalPrice > customerCredits) {
+    return "Insufficient funds!";
+  } else {
+    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+  }
+}
 
 
     
